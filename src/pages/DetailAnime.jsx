f792d04576e91f448dcf505147177cac2e4b1808
= <=>
2025-03-04 00:28:36 +0700

    animedetail

--- src/pages/DetailAnime.jsx
+++ src/pages/DetailAnime.jsx
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Squares from "../components/Squares";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStar,
+  faRankingStar,
+  faBookmark,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 function DetailAnime() {
   const { id } = useParams();
@@ -22,15 +32,44 @@ function DetailAnime() {
   }
 
   if (!anime.mal_id) {
-    return <h1 className="flex justify-center pt-[50vh] md:pt-[50vh] font-black">Anime Tidak Ditemukan Kembali Ke Home..  </h1>;
+    return (
+      <h1 className="flex justify-center pt-[50vh] md:pt-[50vh] font-black">
+        Loading...
+      </h1>
+    );
   }
-  
+
   return (
     <div className="container mx-auto max-w-full">
       <NavBar />
 
-      <header>
+      <div className="absolute -z-40 w-full h-96 md:h-[100vh]">
+        <Squares direction="diagonal" speed={0.5} squareSize={100} />
+      </div>
 
+      <header className="flex flex-col md:flex-row md:text-left md:items-center md:justify-start md:px-20 md:pt-52 text-center items-center gap-6 justify-center pt-40">
+        <Zoom className="">
+          <img
+            src={anime.images.jpg.large_image_url}
+            className="w-40 rounded outline outline-slate-700"
+          />
+        </Zoom>
+        <div className="">
+          <h1 className="text-2xl md:text-4xl font-black pb-4">
+            {anime.title}
+          </h1>
+          <hr className="p-2"></hr>
+          <div className="flex flex-row gap-2 justify-start items-center">
+            <FontAwesomeIcon icon={faClock} />
+            <p>{anime.duration}</p>
+            <FontAwesomeIcon icon={faStar} />
+            <p>{anime.score}</p>
+            <FontAwesomeIcon icon={faRankingStar} />
+            <p>{anime.popularity}</p>
+            <FontAwesomeIcon icon={faBookmark} />
+            <p>{anime.favorites}</p>
+          </div>
+        </div>
       </header>
     </div>
   );
